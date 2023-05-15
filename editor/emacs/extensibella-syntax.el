9b2b77d5a0a1a578ab15a1764872e209175dfea7
~ <micha576@umn.edu>
2023-05-15 11:26:53 -0500

    Fix syntax highlighting to respect word boundaries

--- editor/emacs/extensibella-syntax.el
+++ editor/emacs/extensibella-syntax.el
@@ -6,24 +6,24 @@
   '(
     ;;The actual top commands
     ;; Theorem | Define | Import | Kind | Split | Type | Extensible_Theorem | Module | Prove | Translation_Constraint
-    ("\\<\\(Theorem\\)\\|\\(Define\\)\\|\\(Import\\)\\|\\(Kind\\)\\|\\(Split\\)\\|\\(Type\\)\\|\\(Extensible_Theorem\\)\\|\\(Module\\)\\|\\(Prove\\)\\|\\(Translation_Constraint\\)\\|\\(Prove_Constraint\\)\\|\\(Ext_Ind\\)\\|\\(Prove_Ext_Ind\\)\\>"
+    ("\\<\\(\\(Theorem\\)\\|\\(Define\\)\\|\\(Import\\)\\|\\(Kind\\)\\|\\(Split\\)\\|\\(Type\\)\\|\\(Extensible_Theorem\\)\\|\\(Module\\)\\|\\(Prove\\)\\|\\(Translation_Constraint\\)\\|\\(Prove_Constraint\\)\\|\\(Ext_Ind\\)\\|\\(Prove_Ext_Ind\\)\\)\\>"
      . font-lock-keyword-face)
     ;;Auxiliary words to go with them
     ;; as | by
-    ("\\<\\(as\\)\\|\\(by\\)\\>"
+    ("\\<\\(\\(as\\)\\|\\(by\\)\\)\\>"
      . font-lock-keyword-face)
     ;;Highlighting types
-    ("type"
+    ("\\<\\(type\\)\\>"
      . font-lock-type-face)
     )
   )
 (defconst extensibella-common-commands
   '(
     ;; Set | Show
-    ("\\(Set\\)\\|\\(Show\\)"
+    ("\\<\\(\\(Set\\)\\|\\(Show\\)\\)\\>"
      . font-lock-keyword-face)
     ;;We want this to stand out, since it doesn't belong in scripts
-    ("Quit"
+    ("\\<\\(Quit\\)\\>"
      . font-lock-warning-face)
     )
   )
@@ -33,15 +33,15 @@
     ;; apply | assert | backchain | case | clear | induction | intros | left |
     ;; rename | right | search | split | split* | unfold |
     ;; witness | in | with
-    ("\\<\\(apply\\)\\|\\(assert\\)\\|\\(backchain\\)\\|\\(case\\)\\|\\(clear\\)\\|\\(induction\\)\\|\\(intros\\)\\|\\(left\\)\\|\\(rename\\)\\|\\(right\\)\\|\\(search\\)\\|\\(split\\)\\|\\(split*\\)\\|\\(unfold\\)\\|\\(witness\\)\\|\\(in\\)\\|\\(with\\)\\>"
+    ("\\<\\(\\(apply\\)\\|\\(assert\\)\\|\\(backchain\\)\\|\\(case\\)\\|\\(clear\\)\\|\\(induction\\)\\|\\(intros\\)\\|\\(left\\)\\|\\(rename\\)\\|\\(right\\)\\|\\(search\\)\\|\\(split\\)\\|\\(split*\\)\\|\\(unfold\\)\\|\\(witness\\)\\|\\(in\\)\\|\\(with\\)\\)\\>"
      . font-lock-function-name-face)
     ;;Auxiliary words to go with them
     ;; keep | on | to
-    ("\\<\\(keep\\)\\|\\(on\\)\\|\\(to\\)\\>"
+    ("\\<\\(\\(keep\\)\\|\\(on\\)\\|\\(to\\)\\)\\>"
      . font-lock-function-name-face)
     ;;Commands which we don't want to have appear
     ;; skip | abort | #back | #reset | undo
-    ("\\<\\(skip\\)\\|\\(abort\\)\\|\\(#back\\)\\|\\(#reset\\)\\|\\(undo\\)\\>"
+    ("\\<\\(\\(skip\\)\\|\\(abort\\)\\|\\(#back\\)\\|\\(#reset\\)\\|\\(undo\\)\\)\\>"
      . font-lock-warning-face)
     )
   )
@@ -56,12 +56,12 @@
 (defconst extensibella-logic
   '(
     ;; forall | exists | nabla
-    ("\\<\\(forall\\)\\|\\(exists\\)\\|\\(nabla\\)\\>"
+    ("\\<\\(\\(forall\\)\\|\\(exists\\)\\|\\(nabla\\)\\)\\>"
      ;;This needs to use font-lock-keyword-face because 'exists' is in
      ;;both this and proof commands
      . font-lock-keyword-face)
     ;; true | false
-    ("\\<\\(true\\)\\|\\(false\\)\\>"
+    ("\\<\\(\\(true\\)\\|\\(false\\)\\)\\>"
      . font-lock-builtin-face)
     )
   )
@@ -78,7 +78,7 @@
      . font-lock-warning-face)
     ;;Errors in the output
     ;; Error: | Syntax error. | Search failed | Warning:
-    ("\\<\\(Error:\\)\\|\\(Syntax error.\\)\\|\\(Search failed\\)\\|\\(Warning:\\)\\>"
+    ("\\<\\(\\(Error:\\)\\|\\(Syntax error.\\)\\|\\(Search failed\\)\\|\\(Warning:\\)\\)\\>"
      . font-lock-warning-face)
     ;;Highlight debug output demarcation to separate it from the actual output
     ("~+ .+ ~+"
@@ -96,7 +96,7 @@
     ("Variables:"
      . font-lock-keyword-face)
     ;; Subgoal | is: | other subgoals | other subgoal
-    ("\\<\\(Subgoal\\)\\|\\(is:\\)\\|\\(other subgoals\\)\\|\\(other subgoal\\)\\>"
+    ("\\<\\(\\(Subgoal\\)\\|\\(is:\\)\\|\\(other subgoals\\)\\|\\(other subgoal\\)\\)\\>"
      . font-lock-keyword-face)
     ;; Theorem
     ("Theorem"
