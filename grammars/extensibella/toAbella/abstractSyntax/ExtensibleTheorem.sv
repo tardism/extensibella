b53c2365612593db1e492045cbcb065b18be7d4b
~ <demichaelson@gmail.com>
2024-01-01 16:43:36 -0600

    Fix alsos length check to >0 in Prove as well

--- grammars/extensibella/toAbella/abstractSyntax/ExtensibleTheorem.sv
+++ grammars/extensibella/toAbella/abstractSyntax/ExtensibleTheorem.sv
@@ -252,7 +252,7 @@ top::TopCommand ::= names::[QName]
       map(anyProofCommand,
           head(thms.duringCommands).2); --intros for first thm
   local fullThms::Metaterm =
-      if alsos.len > 1
+      if alsos.len > 0
       then andMetaterm(thms.toAbella, alsos.toAbella)
       else thms.toAbella;
 
